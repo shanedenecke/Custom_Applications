af614abb0ebbc0c2afa0febf1bc9071ec4a7cfc3
shanedenecke <shanedenecke@gmail.com>
2020-03-24 15:37:39 +0200

    initial commit

---
+++ BUSCO_parse.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 10:12:36 2020
+
+BUSCO output parser
+
+@author: shanedenecke
+"""
+### Packages
+import os
+import pandas as pd
+import argparse
+import re
+import sys
+
+
+#### set home directory
+#os.chdir('/home/shanedenecke/Dropbox/quick_temp/clean_summary')
+
+def list_to_df(x):
+    k=[]
+    v=[]
+    for j in x:
+        sublist=j.split(':')
+        k.append(sublist[0])
+        v.append([sublist[1]])
+    final_data=pd.DataFrame(data=dict(zip(k,v)))
+    return(final_data)
+
+
+
+################################################## Read in ARGS
+CLI=argparse.ArgumentParser()
+CLI.add_argument("-file",type=str,help='Summary output file from BUSCO')
+CLI.add_argument("-dir",type=str,help='Directory containing BUSCO outputs')
+args = CLI.parse_args()
+
+
+if args.dir:
+    os.chdir(args.dir)
+    blank=pd.DataFrame()
+    log=[]
+    for i in [x for x in os.listdir() if 'txt' in x]:
+        spname=i[0:6]
+        try:
+            raw=[line.rstrip('\n') for line in open(i)][7]
+        except:
+            log.append('Cannot read in '+i+' File')
+            pass
+        values=re.findall(r'[A-Z].[0-9]+\.[0-9]',raw)
+        values.append('Species:'+spname)
+        sp_dataframe=list_to_df(values)
+        blank=blank.append(sp_dataframe,ignore_index=True)
+
+    blank.columns=['Completeness','Single_copy','Duplicated','Fragmented','Missing','Species']
+    blank.to_csv(sys.stdout,index=False,sep='\t')
+elif args.file:
+    i=args.file
+    b=os.path.basename(i)
+    blank=pd.DataFrame()
+    spname=b[0:6]
+    try:
+        raw=[line.rstrip('\n') for line in open(i)][7]
+    except:
+        print('Cannot read in '+i+' File')
+        pass 
+    values=re.findall(r'[A-Z].[0-9]+\.[0-9]',raw)
+    values.append('Species:'+spname)
+    sp_dataframe=list_to_df(values)
+    blank=blank.append(sp_dataframe,ignore_index=True)
+    blank.to_csv(sys.stdout,header=None,index=False,sep='\t')
