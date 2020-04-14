e643d6038919888d991f753c3b9ae256b8b9d991
shanedenecke <shanedenecke@gmail.com>
2020-04-14 23:37:57 +0300

    many updates

--- Species_phylogeny.sh
+++ Species_phylogeny.sh
@@ -32,17 +32,17 @@ done
 
 
 #Set up directory tree
-base=$(echo $(basename $taxids))
+base=$(echo $(basename $taxids | cut -f 1 -d '.'))
 mkdir $base
 mkdir $base/rax_output
 mkdir $base/one_to_one
 
 ### output one to one orthologues either from orthoDB or OrthoFinder
 if[ $algo = 'OrthoDB' ]; then
-  ~/Applications/Custom_Applications/one_to_one_ID_exec.py -taxid $taxids -output seq -outdir $base/one_to_one
+  ~/Applications/Custom_Applications/odb_parse.py -taxid $taxids -output seq --outdir $base/one_to_one --maxseqs 200
 elif[ $algo = 'Orthofinder' ] 
   mkdir $base/tempseqs
-  mkdir $base/orthofinder_temp
+  mkdir $base/orthofinder_temp ### COULD ADD IN A CONVERSION FROM TAXID TO SPECIES
   cat $taxids | while read i; do cp ~/Applications/Custom_Applications/OrthoDB_source/species_proteomes/$i'_unigene.faa' ./tempseqs/$i'_unigene.faa'; done 
   ~/Applications/Orthofinder/orthofinder -f tempseqs -o orthofinder_temp
   cat tempseqs/*.faa > tempseqs/total_proteome.faa
@@ -50,17 +50,20 @@ elif[ $algo = 'Orthofinder' ]
 fi
 
 ### perform alignments for all one to ones 
-for x in  $output_directory/*.faa
+for x in  $base/one_to_one/*.faa
 do
   cat $x | sed 's/\./_/g' | mafft --quiet --thread $THREADS - > $x'.aln'
   ~/Applications/trimal/source/trimal -automated1 -phylip_paml -in $x.aln -out $x.phy
 done
   
-#### merge all phylip files
-Rscript ~/Applications/Custom_Applications/Phylip_merge.R $base/one_to_one > $base/one_to_one'/Full_species.phy'
+#### merge all phylip files   
+Rscript ~/Applications/Custom_Applications/Phylip_merge.R $base/one_to_one/ > $base/one_to_one/Full_species.phy
 sed -i 's/J/A/g' $base/one_to_one'/Full_species.phy'
 sed -i 's/\./A/g' $base/one_to_one'/Full_species.phy'
 
 ### Perform RaxML tree
-~/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 100 -T $THREADS -m PROTGAMMAAUTO -s $x.phy -n $b.nwk -w mkdir $base/rax_output -o $outgroups
-
+if[ $outgroups = 'None|none' ]; then
+  ~/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 100 -T $THREADS -m PROTGAMMAAUTO -s $base/one_to_one'/Full_species.phy' -n $base.nwk -w $base/rax_output -o $outgroups
+else
+  ~/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 100 -T $THREADS -m PROTGAMMAAUTO -s $base/one_to_one'/Full_species.phy' -n $base.nwk -w $base/rax_output
+fi
