cd7da89b8358ea580c42a75728977e0b2339b8d7
shanedenecke <shanedenecke@gmail.com>
2020-06-08 22:23:49 +0300

    updated directories

--- align_and_tree.sh
+++ align_and_tree.sh
@@ -22,9 +22,8 @@ mkdir $base
 sed 's/-/_/g' $1 >  $base'/clean_'$base'.faa'
 
 mafft --thread $2 $base'/clean_'$base'.faa' > $base'/'$base'.aln'
-~/Applications/source/trimal -in $base'/'$base'.aln' -out $base'/'$base'.aln.trimm'
-~/Applications/custom/fasta_2_phylip.sh $base'/'$base'.aln.trimm' > $base'/'$base'.aln.trimm.phy'
-Rscript ~/Applications/custom/Phylip_duplicate.R $base
+~/Applications/trimal/source/trimal -automated1 -phylip_paml  -in $base'/'$base'.aln' -out $base'/'$base'.aln.trimm.phy'
+Rscript ~/Applications/Custom_Applications $base
 
 
 raxfile=$base'/'$base'.aln.trimm.phy'
@@ -35,10 +34,10 @@ raxdir=$(pwd)'/'$base'/'
 if [ ! -z $3 ]  ]
    then
     echo 'without outgroup'
-    /data2/shane/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 500 -T $2 -m PROTGAMMAAUTO -s $raxfile -n $base'.tre' -w $raxdir
+   ~/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 500 -T $2 -m PROTGAMMAAUTO -s $raxfile -n $base'.tre' -w $raxdir
 
  else
     echo 'include outgroup'
-    /data2/shane/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 500 -T $2 -m PROTGAMMAAUTO -s $raxfile -n $base'.tre' -w $raxdir -o $3
+    ~/Applications/raxml/raxmlHPC-PTHREADS-AVX -f a -x 12345 -p 12345 -N 500 -T $2 -m PROTGAMMAAUTO -s $raxfile -n $base'.tre' -w $raxdir -o $3
  fi
   
